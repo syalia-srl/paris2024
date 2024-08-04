101a2b7af0ed68f5d46b355509fee4bb11c46ac3
Yudivián <yudivian@gmail.com>
2024-08-03 22:20:14 -0400

    chaging menu order

--- menu.py
+++ menu.py
@@ -6,6 +6,6 @@ def menu():
     st.sidebar.page_link("pages/Predictions_Stats.py",label="Estadísticas")
     st.sidebar.page_link("pages/Medal_Table.py", label="Tabla de Medallas")
     st.sidebar.page_link("pages/Countries.py", label="Países")
-    st.sidebar.page_link("pages/Team.py", label="Equipo")
     st.sidebar.page_link("pages/Ranking.py", label="Ranking de Modelos")
     st.sidebar.page_link("pages/Others.py", label="Ranking de Pronósticos")
+    st.sidebar.page_link("pages/Team.py", label="Equipo")
